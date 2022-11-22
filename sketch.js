cc325f9cf5e5c878e985c0155cdf572cb2e2b34d
Lucapedro99 <luca1.pedroni@mail.polimi.it>
2022-11-22 15:31:49 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,64 @@
-function preload() {
-	// put preload code here
-}
+let permissionGranted = false;
+let cx,cy;
+
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+
+  cx = width/2;
+  cy = height/2;
+	
+if(typeof(DeviceOrientationEvent) !== 'undefind' && typeof(DeviceOrientationEvent.requestPermission) === 'function') {
+	
+	//ios 13 device 
+	DeviceOrientationEvent.requestPermission()
+	.catch(() => {
+		// show permission button only first time 
+		let button = createButton("click to allow access to sensors");
+		button = createButton("click to allow to sensors");
+	button.style("font-size","24px");
+	button.center();
+	button.mousePressed( requestAccess );
+	throw error;
+	})
+	.then(() => {
+		permissionGranted = true;
+	})
+ } else {
+	//non ios 13 device
+	textSize(48);
+	text("non ios 13 device",100,100);
+ }
+}
+function requestAccess(){
+	DeviceOrientationEvent.requestPermission()
+	.then(response =>{
+		if (response = 'garanted') {
+			permissionGaranted = true;
+		} else {
+			permissionGranted = false;
+		}
+	})
+	.cartch(console.error);
+
+	button.remove();
 }
 
 function draw() {
 	// put drawing code here
 
-	circle(20,20,20);
+	if (!permissionGranted) return;
+
+	//background(255);
+
+	// rotation X / Y 
+    const dx = constrain(rotationX, -3, 3)
+	const dy = constrain(rotationY, -3, 3)
+	cx += dx*2;
+	cy += dy*2;
+	cx = constrain(cx, 0 , width);
+	cy = constrain(cy, 0 , height);
+
+	rect(cx,cy, 20, 20);
+
 }
